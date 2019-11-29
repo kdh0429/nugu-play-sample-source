32455b572674fb2e04bebb12e85212f2e865a02d
kdh0429 <kdh93429@gmail.com>
2019-11-29 18:19:43 +0900

    .

--- nugu/index.js
+++ nugu/index.js
@@ -2,24 +2,6 @@ const uuid = require('uuid').v4
 const _ = require('lodash')
 const { DOMAIN } = require('../config')
 
-function throwDice(diceCount) {
-  const results = []
-  let midText = ''
-  let resultText = ''
-  let sum = 0
-  console.log(`throw ${diceCount} times`)
-  for (let i = 0; i < diceCount; i++) {
-    const rand = Math.floor(Math.random() * 6) + 1
-    console.log(`${i + 1} time: ${rand}`)
-    results.push(rand)
-    sum += rand
-    midText += `${rand}, `
-  }
-
-  midText = midText.replace(/, $/, '')
-  return {midText, sum, diceCount}
-}
-
 class NPKRequest {
   constructor (httpReq) {
     this.context = httpReq.body.context
@@ -38,24 +20,62 @@ class NPKRequest {
     const actionName = this.action.actionName
     const parameters = this.action.parameters
 
-    switch (actionName) {
-    case 'ThrowDiceAction' || 'ThrowYesAction':
-      let diceCount = 1
-      if (!!parameters) {
-        const diceCountSlot = parameters.diceCount
-        if (parameters.length != 0 && diceCountSlot) {
-          diceCount = parseInt(diceCountSlot.value)
-        }
-
-        if (isNaN(diceCount)) {
-          diceCount = 1
-        }
-      }
-      const throwResult = throwDice(diceCount)
-      npkResponse.setOutputParameters(throwResult)
+    switch (actionName) { 
+    case `/nugu/answer.opponent.specific`:
+      npkResponse.answerOpponentChampionWinningRateAndTear()
       break
-    case 'answer.opponent.caution_champion':
+    case `/nugu/answer.opponent.caution_champion`:
       npkResponse.answerCautionChampion()
+      break
+    case `/nugu/answer.spell.specific_champion.specific_spell`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/answer.spell.specific_champion.wo_specific_spell`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/answer.spell.all`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/recommend_champion_opponent`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/recommend_champion_default`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/recommend.item.specific_core`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/recommend.item.all`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/recommend.skill.specific`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/recommend.skill.all`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
+      break
+    case `/nugu/write.used_spell`:
+      data1 = ''
+      data2 = ''
+      data3 = ''
       break
     }
   }
@@ -71,17 +91,17 @@ class NPKResponse {
     this.directives = []
   }
 
-  setOutputParameters(throwResult) {
-
+  answerOpponentChampionWinningRateAndTear(){
+    // CRAWLING CODE HAS TO BE IMPLEMENTED HERE
+    // Return : 상대 챔피언 승률, 티어
     this.output = {
-      diceCount: throwResult.diceCount,
-      sum: throwResult.sum,
-      midText: throwResult.midText,
+      OPPONENT_CHAMPION_TEAR: '골드',
+      OPPONENT_CHAMPION_WINNING_RATE: '86',
     }
   }
-
   answerCautionChampion() {
-
+    // CRAWLING CODE HAS TO BE IMPLEMENTED HERE
+    // Return: 상대팀 중 제일 잘하는 챔피언 이름, 승률, 티어
     this.output = {
       OPPONENT_CAUTION_CHAMPION: '애쉬',
       OPPONENT_CHAMPION_TEAR: '골드',
