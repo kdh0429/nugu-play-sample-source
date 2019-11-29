c851103dd760582c4a6de3eb97220bd69a4b9495
kdh0429 <kdh93429@gmail.com>
2019-11-29 15:51:38 +0900

    .

--- nugu/index.js
+++ nugu/index.js
@@ -55,7 +55,7 @@ class NPKRequest {
       npkResponse.setOutputParameters(throwResult)
       break
     case 'answer.opponent' || 'answer.opponent.caution_champion':
-      npkResponse.answerCautionChampion(throwResult)
+      npkResponse.answerCautionChampion()
       break
     }
   }
